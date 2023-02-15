0dafaf3fc22a0db0acf938cc1cf4ecd5435e7d8b
Joao <joaokavalciuki@hotmail.com>
2023-02-15 14:49:04 -0300

    Button functionality added

--- app/src/main/java/com/example/androidbasics/MainActivity.kt
+++ app/src/main/java/com/example/androidbasics/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.androidbasics
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btnAdd : Button = findViewById(R.id.btnAdd)
+        btnAdd.setOnClickListener {
+            val firstName = findViewById<EditText>(R.id.etFirstName).text.toString()
+            val lastName = findViewById<EditText>(R.id.etLastName).text.toString()
+            val birthDate = findViewById<EditText>(R.id.etBirthDate).text.toString()
+            val country =findViewById<EditText>(R.id.etCountry).text.toString()
+            Log.i("MainActivity", "Name: $firstName $lastName, born on: $birthDate, nationality: $country")
+        }
     }
 }
