86bb4efe25539618c48d30c1abadfdc2df36abc7
Joao <joaokavalciuki@hotmail.com>
2023-02-22 23:19:42 -0300

    Raio Buttons and Check boxes manipulationm

--- app/src/main/java/com/example/androidbasics/MainActivity.kt
+++ app/src/main/java/com/example/androidbasics/MainActivity.kt
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import android.widget.TextView
 
 
@@ -13,10 +16,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val btnAddImage : Button = findViewById(R.id.btnAddImage)
-        btnAddImage.setOnClickListener {
-            val image : ImageView = findViewById(R.id.ivImage)
-            image.setImageResource(R.drawable.traintrip)
+        val btnOrder : Button = findViewById(R.id.btnOrder)
+        btnOrder.setOnClickListener {
+            val meatsRadioGroup = findViewById<RadioGroup>(R.id.rgMeat)
+            val meat = findViewById<RadioButton>(meatsRadioGroup.checkedRadioButtonId)
+            val cheese = findViewById<CheckBox>(R.id.cbCheese).isChecked
+            val salad = findViewById<CheckBox>(R.id.cbSalad).isChecked
+            val onions = findViewById<CheckBox>(R.id.cbOnions).isChecked
+            val orderString = "Your order is a hamburger with:\n" +
+                    "${meat.text}\n" +
+                    (if(cheese)  "Cheese\n" else "") +
+                    (if(salad) "Salad\n" else "") +
+                    (if (onions) "Onions" else "")
+            val textView = findViewById<TextView>(R.id.tvOrder)
+            textView.text = orderString
         }
     }
 }
