5f263a9e8c6278638450bb89a23f1283ba0e05cb
Joao <joaokavalciuki@hotmail.com>
2023-06-22 17:21:31 -0300

    Spinner with runtime itens

--- app/src/main/java/com/example/androidbasics/MainActivity.kt
+++ app/src/main/java/com/example/androidbasics/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.AdapterView.OnItemSelectedListener
+import android.widget.ArrayAdapter
 import android.widget.Spinner
 import android.widget.Toast
 
@@ -13,13 +14,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val fatecsSpinner: Spinner = findViewById(R.id.spFatecs)
-        fatecsSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+        val departureOptions = listOf("Depart now", "Set departure time", "Set arrive time",  "Latest Departure")
+        val adapterDepartures = ArrayAdapter<String>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, departureOptions)
+        val spinner: Spinner = findViewById(R.id.spFatecs)
+        spinner.adapter = adapterDepartures
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
 
             override fun onItemSelected(adapterView: AdapterView<*>?, view: View?, position: Int, id: Long) {
                 // @MainActivity is used here because the Toast is inside of the anonymous class, not the activity
                 Toast.makeText(this@MainActivity,
-                    "Fatec ${adapterView?.getItemAtPosition(position).toString()} selecionada",
+                    "${adapterView?.getItemAtPosition(position).toString()} selected",
                     Toast.LENGTH_SHORT).show()
             }
 
