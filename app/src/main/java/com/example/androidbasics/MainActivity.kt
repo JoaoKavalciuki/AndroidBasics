2ae6fba119a60c2c00352e449132153baec673de
Joao <joaokavalciuki@hotmail.com>
2023-07-01 18:45:27 -0300

    Added navigation toolbar make transactions between the fragments

--- app/src/main/java/com/example/androidbasics/MainActivity.kt
+++ app/src/main/java/com/example/androidbasics/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.androidbasics
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.androidbasics.fragments.FirstFragment
 import com.example.androidbasics.fragments.SecondFragment
+import com.example.androidbasics.fragments.ThirdFragment
 
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -12,29 +14,28 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        setSupportActionBar(findViewById(R.id.toolbar))
         val firstFragment = FirstFragment()
         val secondFragment = SecondFragment()
+        val thirdFragment = ThirdFragment()
 
-        supportFragmentManager.beginTransaction().apply{
-            replace(R.id.flFragment, firstFragment)
-            addToBackStack("First Fragment")
+        setCurrentFragment(firstFragment)
+
+        bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.miHome -> setCurrentFragment(firstFragment)
+                R.id.miBooks -> setCurrentFragment(secondFragment)
+                R.id.miAccount -> setCurrentFragment(thirdFragment)
+            }
+            true
+        }
+    }
+
+    private fun setCurrentFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment, fragment)
+            addToBackStack(null)
             commit()
-        }
-
-        btnFragment1.setOnClickListener {
-            supportFragmentManager.beginTransaction().apply{
-                replace(R.id.flFragment, firstFragment)
-                addToBackStack("Second Fragment")
-                commit()
-            }
-        }
-
-        btnFragment2.setOnClickListener {
-            supportFragmentManager.beginTransaction().apply {
-                replace(R.id.flFragment, secondFragment)
-                commit()
-            }
         }
     }
 }
---
+++ app/src/main/java/com/example/androidbasics/fragments/ThirdFragment.kt
@@ -0,0 +1,7 @@
+package com.example.androidbasics.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.androidbasics.R
+
+class ThirdFragment: Fragment(R.layout.fragment_third) {
+}
