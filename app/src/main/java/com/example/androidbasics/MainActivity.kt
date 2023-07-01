2a60a45c93b26b7a1bfab90f1b8cdd3a44a45aeb
Joao <joaokavalciuki@hotmail.com>
2023-07-01 00:29:04 -0300

    Static Fragments

--- app/src/main/java/com/example/androidbasics/MainActivity.kt
+++ app/src/main/java/com/example/androidbasics/MainActivity.kt
@@ -5,36 +5,14 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.androidbasics.models.PcComponent
+
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var pcComponentAdapter : PcComponentAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        initRecyclerView()
-        addDataSource()
-    }
-
-    private fun addDataSource() {
-        val dataSource = DataSource.createDataSet()
-
-        this.pcComponentAdapter.setDataSet(dataSource)
-    }
-
-    private fun initRecyclerView(){
-        this.pcComponentAdapter = PcComponentAdapter{ pcComponent ->
-            openLink(pcComponent)
-        }
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = this.pcComponentAdapter
-    }
-
-    private fun openLink(pcComponent: PcComponent){
-        val url = Intent(Intent.ACTION_VIEW, Uri.parse(pcComponent.buyLink))
-        startActivity(url)
     }
 }
