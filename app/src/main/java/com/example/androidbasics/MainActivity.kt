87b05c2ca96aae9c00dba572204eeb0ebccf2e31
Joao <joaokavalciuki@hotmail.com>
2023-07-03 16:20:19 -0300

    Add tab layout attached to the view pager

--- app/src/main/java/com/example/androidbasics/MainActivity.kt
+++ app/src/main/java/com/example/androidbasics/MainActivity.kt
@@ -2,7 +2,12 @@ package com.example.androidbasics
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+
 
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -15,5 +20,23 @@ class MainActivity : AppCompatActivity() {
         val adapter = ViewPagerAdapter(BookSource.createBookSet())
         viewPager.adapter = adapter
 
+        TabLayoutMediator(tabLayoutView, viewPager){ tab, position ->
+            tab.text = "Book ${position + 1}"
+            tab.setIcon(R.drawable.ic_book)
+
+        }.attach()
+
+        tabLayoutView.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                Toast.makeText(this@MainActivity, "${tab?.text} selected", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                Toast.makeText(this@MainActivity, "${tab?.text} re-selected", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
