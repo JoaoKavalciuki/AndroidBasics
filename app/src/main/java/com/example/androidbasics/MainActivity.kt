745cbf2178940b9d3438dd7844b97d3cb43b1356
Joao <joaokavalciuki@hotmail.com>
2023-07-04 23:28:43 -0300

    Using shared preferences with a simple example

--- app/src/main/java/com/example/androidbasics/MainActivity.kt
+++ app/src/main/java/com/example/androidbasics/MainActivity.kt
@@ -2,50 +2,49 @@ package com.example.androidbasics
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.view.MenuItem
 import android.widget.Toast
-import androidx.appcompat.app.ActionBarDrawerToggle
+
 
 
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var toggle: ActionBarDrawerToggle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.menu_opened, R.string.menu_closed)
-        drawerLayout.addDrawerListener(toggle)
-        toggle.syncState()
+        val sharedPrefence = getSharedPreferences("privateSharedPref", MODE_PRIVATE)
 
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        val delay: Long = 1000
+        val sharedPrefEditor = sharedPrefence.edit()
 
-        navView.setNavigationItemSelectedListener {
-            when(it.itemId){
-                R.id.miAccount ->
-                    Toast.makeText(this, "My Account clicked", Toast.LENGTH_SHORT).show()
-                R.id.miBook ->
-                    Toast.makeText(this, "My Books clicked", Toast.LENGTH_SHORT).show()
-                R.id.miLogout -> {
-                    Toast.makeText(this, "You clicked the logout button. The app wll be closed", Toast.LENGTH_SHORT).show()
-                    Handler(Looper.getMainLooper()).postDelayed({
-                        finish()
-                    }, delay)
+        btnSave.setOnClickListener {
+            val name = etName.text.toString()
+            val occupation = etOccupation.text.toString()
+
+            try{
+                val age = etAge.text.toString().toInt()
+                sharedPrefEditor.apply{
+                    putString("name", name)
+                    putString("occupation", occupation)
+                    putInt("age", age)
+                    apply()
                 }
-
+                Toast.makeText(this, "Your data has been saved", Toast.LENGTH_SHORT).show()
+            } catch (error: IllegalArgumentException){
+                Toast.makeText(this, "This field must be filled with numbers only", Toast.LENGTH_SHORT).show()
+                throw IllegalArgumentException("This field must be filled with numbers only")
             }
-            true
         }
-    }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if(toggle.onOptionsItemSelected(item)) return  true
+        btnLoad.setOnClickListener {
+            val name = sharedPrefence.getString("name", null)
+            val occupation = sharedPrefence.getString("occupation", null)
+            val age = sharedPrefence.getInt("äge", 0)
 
-        return super.onOptionsItemSelected(item)
+            etName.setText(name)
+            etOccupation.setText(occupation)
+            etAge.setText(age.toString())
+        }
     }
 }
