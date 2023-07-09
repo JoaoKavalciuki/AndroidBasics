935b5fa117b15e75fda39b4b333e9c3cc33fec94
Joao <joaokavalciuki@hotmail.com>
2023-07-08 23:27:34 -0300

    Service for playing a song

--- app/src/main/java/com/example/androidbasics/MainActivity.kt
+++ app/src/main/java/com/example/androidbasics/MainActivity.kt
@@ -2,14 +2,11 @@ package com.example.androidbasics
 
 
 
-import android.content.ClipData
-import android.content.ClipDescription
+
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.DragEvent
-import android.view.View
-import android.widget.LinearLayout
-import android.widget.Toast
+import com.example.androidbasics.service.PlaySongService
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -17,58 +14,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        llTop.setOnDragListener(dragListener)
-        llBottom.setOnDragListener(dragListener)
 
-        dragableView.setOnLongClickListener {
-            val clipText = "View moved clipData"
-            val clipItem = ClipData.Item(clipText)
-            val mimeTypes = arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN)
-            val clipData = ClipData(clipText, mimeTypes, clipItem)
-            val dragShadowBuilder = View.DragShadowBuilder(it)
-            it.startDragAndDrop(clipData,  dragShadowBuilder, it, 0)
-
-            it.visibility = View.INVISIBLE
-            true
+        btnStart.setOnClickListener {
+            startService(Intent(this, PlaySongService::class.java))
         }
 
-
-
-    }
-
-    private val dragListener = View.OnDragListener { view, dragEvent ->
-        when(dragEvent.action){
-            DragEvent.ACTION_DRAG_STARTED -> {
-                dragEvent.clipDescription.hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN)
-            }
-            DragEvent.ACTION_DRAG_ENTERED -> {
-                view.invalidate()
-                Toast.makeText(this@MainActivity, "A View chegou ao layout destinatário", Toast.LENGTH_SHORT).show()
-            }
-            DragEvent.ACTION_DRAG_LOCATION -> true
-            DragEvent.ACTION_DRAG_EXITED ->{
-                view.invalidate()
-                Toast.makeText(this@MainActivity, "A View saiu do layout remetente", Toast.LENGTH_SHORT).show()
-            }
-            DragEvent.ACTION_DROP ->{
-                val clipItem = dragEvent.clipData.getItemAt(0)
-                val draggedData = clipItem.text
-                Toast.makeText(this, draggedData, Toast.LENGTH_SHORT).show()
-                view.invalidate()
-
-                val draggedView = dragEvent.localState as View
-                val draggedViewSender = draggedView.parent as LinearLayout
-                draggedViewSender.removeView(draggedView)
-                val draggedViewDestination = view as LinearLayout
-                draggedViewDestination.addView(draggedView)
-
-                draggedView.visibility = View.VISIBLE
-                true
-            }
-            DragEvent.ACTION_DRAG_ENDED -> view.invalidate()
-            else -> false
+        btnStop.setOnClickListener {
+            stopService(Intent(this, PlaySongService::class.java))
         }
-        true
     }
-
 }
