d73f018d3ee5a12d7373e3d0f975871579b16b78
Joao <joaokavalciuki@hotmail.com>
2023-09-15 12:36:55 -0300

    MVVM init

--- app/src/main/java/com/example/androidbasics/MainActivity.kt
+++ app/src/main/java/com/example/androidbasics/MainActivity.kt
@@ -1,12 +1,7 @@
 package com.example.androidbasics
 
-
-
-
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.androidbasics.service.PlaySongService
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,13 +9,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        btnStart.setOnClickListener {
-            startService(Intent(this, PlaySongService::class.java))
-        }
-
-        btnStop.setOnClickListener {
-            stopService(Intent(this, PlaySongService::class.java))
-        }
     }
 }
