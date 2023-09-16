f79eedcf5e192d7889b7818bb3bc08acf0fa38ff
Joao <joaokavalciuki@hotmail.com>
2023-09-16 10:02:16 -0300

    Main View Model and it's factory done

---
+++ app/src/main/java/com/example/androidbasics/viewmodel/main/MainViewModelFactory.kt
@@ -0,0 +1,16 @@
+package com.example.androidbasics.viewmodel.main
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.androidbasics.repositories.MainRepository
+import java.lang.IllegalArgumentException
+
+class MainViewModelFactory(private val repository: MainRepository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return if(modelClass.isAssignableFrom(MainViewModel::class.java)){
+            MainViewModel(this.repository) as T
+        } else{
+            throw  IllegalArgumentException("View Model not found")
+        }
+    }
+}
