7705a8e0b5ac38b550aa252396868527c2cd1ea8
Joao <joaokavalciuki@hotmail.com>
2023-09-17 14:53:47 -0300

    View Binding init

--- app/src/main/java/com/example/androidbasics/repositories/MainRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.example.androidbasics.repositories
-
-import com.example.androidbasics.services.RetrofitService
-
-class MainRepository(private val retrofitService: RetrofitService) {
-    fun getAllLives() = retrofitService.getAllLives()
-}
