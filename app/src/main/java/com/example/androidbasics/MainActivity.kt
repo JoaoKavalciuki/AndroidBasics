cb0360858e796dd535da4821fc2f1a7134260248
Aluno <Aluno@fatecdiadema.com>
2023-09-18 11:08:08 -0300

    Room Database init

--- app/src/main/java/com/example/androidbasics/MainActivity.kt
+++ app/src/main/java/com/example/androidbasics/MainActivity.kt
@@ -4,17 +4,12 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.androidbasics.adapters.MainAdapter
+
 import com.example.androidbasics.databinding.ActivityMainBinding
-import com.example.androidbasics.models.LanguagesMockDataSet
-import com.example.androidbasics.models.MobileLanguage
 
 
 class MainActivity : AppCompatActivity() {
 
-
-    private lateinit var mainAdapter: MainAdapter
     private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,33 +17,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         this.binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-    }
-
-    override fun onStart() {
-        super.onStart()
-        initRecyclerView()
-        addDataSource()
-    }
-
-    private fun initRecyclerView(){
-        mainAdapter = MainAdapter({mobileLanguage ->
-            openLink(mobileLanguage.link)
-        }, this.binding)
-
-        binding.rvLanguages.apply{
-            adapter = mainAdapter
-            layoutManager = LinearLayoutManager(this@MainActivity)
-        }
-    }
-
-    private fun openLink(url: String){
-        val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
-        startActivity(browserIntent)
-    }
-    private fun addDataSource() {
-
-        val dataSet = LanguagesMockDataSet.createDataSet()
-        mainAdapter.setLanguagesList(dataSet)
     }
 
 }
--- app/src/main/java/com/example/androidbasics/models/MobileLanguage.kt
+++
@@ -1,9 +0,0 @@
-package com.example.androidbasics.models
-
-data class MobileLanguage(
-    var language: String,
-    var developer: String,
-    var releaseYear: Int,
-    var link: String,
-    var image: String
-)
